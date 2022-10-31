edd321c6d1b9cdd11a3e993c30f3bda232b75d5a
thikei <taeheek.o0o@gmail.com>
2022-10-31 22:04:20 +0900

    Game

--- SampleQuiz/SampleQuiz/QuizViewController.swift
+++ SampleQuiz/SampleQuiz/QuizViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    var csvArray: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
 
         // Do any additional setup after loading the view.
     }
@@ -19,6 +24,18 @@ class QuizViewController: UIViewController {
     //ボタンを押したときに呼ばれる
     @IBAction func btnAction(sender: UIButton) {
         print(sender.tag)
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+        } catch {
+            print("エラー")
+        }
+        return csvArray
     }
     /*
     // MARK: - Navigation
